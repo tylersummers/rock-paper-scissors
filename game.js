c0e4c9061cf37f5e82be69d69f35500b15ca196b
tylersummers <162672490+tylersummers@users.noreply.github.com>
2025-01-22 16:31:18 +0900

    Add round logic

--- game.js
+++ game.js
@@ -1,10 +1,11 @@
+// Get Computer Choice and Human Choice
 function getComputerChoice() {
-    let i = Math.random();
+    let computerChoice = Math.random();
     console.log("The computer has made it's decision.")
-    if (i <= 0.33) {
+    if (computerChoice < 0.33) {
         return "Paper"
     } 
-    else if (i >= 0.66) {
+    else if (computerChoice < 0.66) {
         return "Rock"
     }   
     else {
@@ -15,10 +16,10 @@ function getComputerChoice() {
 getComputerChoice();
 
 function getHumanChoice () {
-    let HumanChoice = prompt("What will it be?").toLowerCase()
-    if (HumanChoice === "rock" || HumanChoice === "paper" || HumanChoice === "scissors") {
+    let humanChoice = prompt("What will it be?").toLowerCase()
+    if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
         console.log("You have made your decision.")
-        return
+        return HumanChoice
     }
     else {
         console.log("Invalid input - try again.")
@@ -26,3 +27,25 @@ function getHumanChoice () {
 }
 
 getHumanChoice();
+
+// Set initial score
+let computerScore = 0
+let humanScore = 0
+
+function playRound (computerChoice, humanChoice) {
+    if (computerChoice === humanChoice) {
+        console.log("It's a tie!")
+    }
+
+    else if (computerChoice === "scissors" && humanChoice === "rock" || computerChoice === "rock" && humanChoice === "paper" || computerChoice === "scissors" && humanChoice === "paper") {
+        console.log("You win! Nice.")
+        humanScore++
+    }
+
+    else {
+        console.log("The computer wins! Unlucky.")
+        computerScore++
+    }
+}
+
+playRound(computerChoice, humanChoice);
