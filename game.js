78a0dfdb62d10afc7f2c60b265459a5cea9b319b
tylersummers <162672490+tylersummers@users.noreply.github.com>
2025-02-10 15:57:39 +0900

    Use DOM methods to track round results and score

--- game.js
+++ game.js
@@ -21,49 +21,62 @@ function getHumanChoice() {
     });
 }
 
-// Play Game
+// Run the Game
 
 async function playGame() {
 
     let computerScore = 0
     let humanScore = 0
 
-    function playRound (computerChoice, humanChoice) {
+    // Scoreboard
+
+    const scoreDiv = document.createElement("div")
+    scoreDiv.textContent = "Your Score: " + humanScore + " || Computer Score: " + computerScore
+    scoreDiv.style.margin = "15px"
+    document.body.appendChild(scoreDiv)
+
+    const resultDiv = document.createElement("div")
+    resultDiv.textContent = "Let's rock... or paper... or scissor."
+    resultDiv.style.margin = "15px"
+    document.body.insertBefore(resultDiv, scoreDiv)
+
+    function playRound (computerChoice, humanChoice, roundNumber) {
 
         if (computerChoice === humanChoice) {
-            console.log("It's a tie!")
+            resultDiv.textContent = "Round " + roundNumber + " is a tie!"
         }
     
         else if (computerChoice === "scissors" && humanChoice === "rock" || computerChoice === "rock" && humanChoice === "paper" || computerChoice === "paper" && humanChoice === "scissors") {
-            console.log("You win! Nice.")
+            resultDiv.textContent = "You win round " + roundNumber + ", nice!"
             humanScore++
         }
     
         else {
-            console.log("The computer wins! Unlucky.")
+            resultDiv.textContent = "The computer wins round " + roundNumber + ", oh no!"
             computerScore++
         }
+
+        scoreDiv.textContent = "Your Score: " + humanScore + " || Computer Score: " + computerScore
+
     }
 
 
     for (let i = 0; i < 5; i++) {
         const humanSelection = await getHumanChoice();
         const computerSelection = getComputerChoice();
-        playRound(computerSelection, humanSelection);
+        playRound(computerSelection, humanSelection, i + 1);
     }
 
-    console.log("Game over! Computer score: " + computerScore + " Your score: " + humanScore)
-
         if (computerScore === humanScore) {
-            console.log("The game is a tie!")
+            resultDiv.textContent = "Game over! It's a tie."
         }
 
         else if (computerScore > humanScore) {
-            console.log("You lose the game!")
+            resultDiv.textContent = "Game over! You lose!"
         }
 
         else
-        console.log("You win the game!")
+        resultDiv.textContent = "Game over! You win!"
     }
 
 playGame();
